76f8a4e866ffd5cbb99f6e051582e670bb5c275a
Dmytro Tereshchenko <dmytro_tereshchenko@epam.com>
2022-08-01 20:03:38 +0300

    feat: Integration with import file lambda

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://ysvoq66q83.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://ykm9zb25bg.execute-api.eu-west-1.amazonaws.com/',
+  import: 'https://7717ozxvq3.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
